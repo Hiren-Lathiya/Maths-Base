e47847a95c72dbcff4755bb54da431856e4d9caf
CareTaker <caretaker023@gmail.com>
2021-07-02 09:50:32 +0530

    messaging

---
+++ app/src/main/java/com/hirenroyal/basemaths/MainActivity.java
@@ -0,0 +1,24 @@
+package com.hirenroyal.basemaths;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+
+
+    public void startGame(View view) {
+       Intent intent=new Intent (MainActivity.this,StartGame.class);
+        startActivity(intent);
+        finish();
+    }
+}
